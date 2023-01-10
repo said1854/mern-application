592609e5471cdd7ff0dd0d9d4308568df6733f1e
said1854 <msaliogullari@gmail.com>
2023-01-10 11:33:46 +0300

    route setting, controllers

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,12 @@
-console.log("hello world!")
-console.log("hello said")
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}))
+app.use('/api/goals', require('./routes/goalRoutes.js'));
+
+app.listen(port, () => {
+    console.log(`Server is running on port:${port}`)
+})
