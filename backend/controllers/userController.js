9e45a363aa2a62deb4585e9ce1ddbcc4b1fa477c
said1854 <msaliogullari@gmail.com>
2023-01-11 00:33:19 +0300

    user cont. model. route

---
+++ backend/controllers/userController.js
@@ -0,0 +1,63 @@
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const e = require('express');
+//@desc     Register new user     
+//@route    POST /api/users
+//access    Public
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+        res.status(400)
+        throw new Error('Please fill all fields')
+    }
+    //Check if user exists
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+        res.status(400)
+        throw new Error('User already exists.')
+    }
+    //Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password,salt)
+    //Create user
+    const user = await User.create({
+        name: name,
+        email: email,
+        password: hashedPassword
+    })
+    if (user) {
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            email : user.email
+        })
+    } else {
+        res.status(400);
+        throw new Error('Invalid User Data')
+    }
+    res.json({ user })
+})
+//@desc     Authenticate a user     
+//@route    POST /api/users/login
+//access    Public
+const loginUser = asyncHandler(async (req, res) => {
+    res.json({message:"Login User"})
+})
+//@desc     Get user data     
+//@route    GET /api/users/me
+//access    Private
+const getMe = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id)
+    res.json({user})
+})
+//@desc     Get all users data     
+//@route    GET /api/users
+//access    Public
+const getAllUsers = asyncHandler(async (req, res) => {
+    const users = await User.find()
+    res.json({users})
+})
+
+module.exports = { registerUser,loginUser,getMe,getAllUsers }
